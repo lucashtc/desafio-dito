ac5dc9dce16315b8a694a3dba605c6b43c5146cd
lucashtc <lucashenrique-teixeira@hotmail.com>
2019-09-12 17:57:08 -0300

    Feat: Add procedure para alimentar base com 30000 linhas

--- dito.sql
+++ dito.sql
@@ -1,42 +1,35 @@
-USE `dito`
+USE dito;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `EVENT` varchar(15) NOT NULL DEFAULT '',
-  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=142 DEFAULT CHARSET=utf8;
+  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela dito.events: ~59 rows (aproximadamente)
-/*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` (`ID`, `EVENT`, `TIMESTAMP`) VALUES
-	(2, 'comprou-produto', '2019-09-07 00:05:56'),
-	(3, 'comprou', '2019-09-07 00:06:09'),
-	(83, 'buy', '2019-09-07 11:38:27'),
-	(84, 'buy', '2019-09-07 11:38:27'),
-	(85, 'buy', '2019-09-07 11:38:27'),
-	(86, 'buy', '2019-09-07 11:38:27'),
-	(87, 'buy', '2019-09-07 11:38:28'),
-	(88, 'buy', '2019-09-07 11:38:28'),
-	(89, 'buy', '2019-09-07 11:38:28'),
-	(90, 'buy', '2019-09-07 11:38:28'),
-	(91, 'buy', '2019-09-07 11:38:28'),
-	(92, 'buy', '2019-09-07 11:38:28'),
-	(93, 'buy', '2019-09-07 11:38:28'),
-	(94, 'buy', '2019-09-07 11:38:28'),
-	(95, 'buy', '2019-09-07 11:38:28'),
-	(96, 'buy', '2019-09-07 11:38:28'),
-	(97, 'buy', '2019-09-07 11:38:28'),
-	(98, 'buy', '2019-09-07 11:38:29'),
-	(99, 'buy', '2019-09-07 11:38:29'),
-	(100, 'buy', '2019-09-07 11:38:29'),
-	(101, 'buy', '2019-09-07 11:38:29'),
-	(102, 'buy', '2019-09-07 11:38:29'),
-	(103, 'buy', '2019-09-07 11:38:29'),
-	(104, 'buy', '2019-09-07 11:38:29'),
-	(105, 'buy', '2019-09-07 11:38:29');
-/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+DROP PROCEDURE IF EXISTS `insertFake`;
+DELIMITER //
+CREATE  PROCEDURE `insertFake`()
+BEGIN
+DECLARE i int  DEFAULT  1;
+	WHILE i <= 10000 DO
+		INSERT INTO `events` (`EVENT`, `TIMESTAMP`) VALUES 
+			('comprou-produto', '2019-09-07 00:05:56'),
+			('comprou', '2019-09-07 00:06:09'),
+			('buy', '2019-09-07 11:38:27');
+		SET i = i + 1;
+	END WHILE;
+END//
+DELIMITER ;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+CALL insertFake();
